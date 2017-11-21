935fcef32629cc8fb161e53a3aec38548eef4a57
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-21 16:08:05 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,10 +27,10 @@ def initial_round
   card_total = deal_card + deal_card
   return card_total
 end
-  display_card_total(card_total)
+  display_card_total = card_total
   puts "Your cards add up to #{card_total}"
-end 
-end 
+end
+end
 
 def hit?
   # code hit? here
