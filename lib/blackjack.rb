14bf210f482b96ba57d629f2e46543e2e34a06f3
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-21 17:01:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,10 +41,8 @@ def hit? (card_total)
  end
 end
 
-
-   return
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
